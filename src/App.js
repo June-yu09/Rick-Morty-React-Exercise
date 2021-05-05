9cb41a68a5a1ff73ca9d2377d7410270d099ec80
June-yu09 <yuzatee@gmail.com>
2021-05-05 21:30:16 +0200

    worked on searching

--- src/App.js
+++ src/App.js
@@ -1,44 +1,82 @@
-import { useState, useEffect } from 'react';
-import { Navbar, Form, FormControl } from 'react-bootstrap';
+import React, { useState, useEffect, useRef} from 'react';
 import './App.css';
-import Characters from './components/Characters.js'
-import Loading from './components/Loading.js'
+import Characters from './components/Characters.js';
+import Loading from './components/Loading.js';
+import { Form, Navbar, FormControl } from 'react-bootstrap';
+
 
 function App() {
   let [myData, setData] = useState([]);
   let [myInput, setMyInput] = useState('');
   let [loading, setLoading] = useState(true);
+  let [displayingData, setDisplayingData] = useState([]);
   
+  const inputRef = useRef('');
 
   const fetchAPI = ()=>{
     fetch('https://rickandmortyapi.com/api/character/')
     .then(response=>response.json())
-    .then(data=>setData(data.results))
+    .then(data=>{
+      return data.results
+    })
+    .then(d=>{
+      setData(d);
+      setDisplayingData(d);
+    })
+    
   }
-
+  
   useEffect(()=>{
     fetchAPI();
     setLoading(false);
-    
-  })
+  },[])
+
+
+  function matchingName(){
+    let displayData = [];
+    myData.forEach(obj=>{
+      if (((obj.name).toUpperCase()).includes(myInput.toUpperCase())){
+        displayData.push(obj);
+      }
+    });
+    setDisplayingData(displayData);
+  }
+
+  useEffect(()=>{
+    if(myData){
+      matchingName();
+      console.log("works")
+      console.log(myData);
+    }else{
+      console.log('my data is still empty')
+    }
+  },[myInput])
+
+  useEffect(()=>{
+    inputRef.current.focus();
+  },[])
 
   return (
     <div className="App">
 
-      <Navbar bg="primary" variant="dark">
-          <Form className="searchInput" inline>
-            <FormControl onChange={(e)=>{
-              setMyInput(e.target.value);
-            }} type="text" placeholder="Search" className="mr-sm-2" />
-          </Form>
-      </Navbar>
-
+      <div>
+          <Navbar bg="primary" variant="dark">
+              <Form className="searchInput" inline>
+                  <FormControl ref={inputRef} onChange={(e)=>{
+                    setMyInput(e.target.value);
+                    console.log(e.target.value);
+                  }} type="text" placeholder="Search" className="mr-sm-2" />
+              </Form>
+          </Navbar>
+      </div>
+      { myInput }
+      
       
       <Loading loading={loading} />
+
       
 
-
-      <Characters myData={ myData } />
+      <Characters wholeData={ displayingData } />
       
       
     </div>
--- src/components/Characters.js
+++ src/components/Characters.js
@@ -1,22 +1,29 @@
-import { flipCard } from 'react'
+import '/Users/jiyoonyu/Desktop/coding/Rick_Morty_React_Exercise/src/App.css';
+import { Button } from 'react-bootstrap';
 
 function Characters(props) {
 
     
     return (
         <>
+           
         
 
 
                 {
-                    props.myData.length !== 0?
-                    props.myData.map((d,i)=>{
+                    props.wholeData.length !== 0?
+                    props.wholeData.map((d,i)=>{
                     return (
                         <div className="flipCard">
                         
                                 <div className="flipCardInner">
                                     <div className="flipCardFront">
-                                        <img key={d.id} src={d.image} alt="characterImage"/>
+                                        <img className='characterImage' key={d.id} src={d.image} alt="characterImage"  />
+                                    </div>
+                                    <div className='flipCardBack'>
+                                        <h4> { d.name } </h4>
+                                        <p>Status: { d.status } </p>
+                                        <Button variant="primary">More Details</Button>
 
                                     </div>
 
